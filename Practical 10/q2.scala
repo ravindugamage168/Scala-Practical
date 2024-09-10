96ad84f3fd7c5b040f4cef2e2f53f1f72d70027f
ravindugamage168 <ravindugamage168@gmail.com>
2024-09-10 08:26:10 +0530

    Practical 10 Codes

---
+++ Practical 10/q2.scala
@@ -0,0 +1,48 @@
+class Rational(n: Int, d: Int) {
+  require(d != 0, "Denominator cannot be zero")
+
+  private val g = gcd(n.abs, d.abs)
+  
+  val numerator: Int = n / g
+  val denominator: Int = d / g
+
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+
+  
+  def neg: Rational = new Rational(-numerator, denominator)
+
+  
+  def add(that: Rational): Rational = {
+    new Rational(
+      numerator * that.denominator + that.numerator * denominator,
+      denominator * that.denominator
+    )
+  }
+
+  def sub(that: Rational): Rational = {
+    new Rational(
+      numerator * that.denominator - that.numerator * denominator,
+      denominator * that.denominator
+    )
+  }
+
+  override def toString: String = s"$numerator/$denominator"
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val x = new Rational(3, 4)
+    val y = new Rational(5, 8)
+    val z = new Rational(2, 7)
+
+    val result = y.sub(z)
+
+    val finalResult = x.add(result)
+
+    println(s"x = $x")
+    println(s"y = $y")
+    println(s"z = $z")
+    
+    println(s"Result of x + (y - z) : $finalResult")
+  }
+}
---
+++ Practical 10/q5.scala
@@ -0,0 +1,14 @@
+object LetterCount {
+  def countLetterOccurrences(words: List[String]): Int = {
+    val wordLengths = words.map(_.length)
+    val totalLetters = wordLengths.reduce(_ + _)
+
+    totalLetters
+  }
+
+  def main(args: Array[String]): Unit = {
+    val words = List("apple", "banana", "cherry", "date")
+    val total = countLetterOccurrences(words)
+    println(s"Total count of letter occurrences: $total")
+  }
+}
